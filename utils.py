c77d5e50452a757652632e9958ec8d1f0ab461ad
Pranav Sastry <pranava.sri@gmail.com>
2023-02-09 15:03:46 +0530

    added initial code for utils, venue, decision

--- utils.py
+++ utils.py
@@ -1,1 +1,26 @@
-# will contain the login and papers2csv all such mic utils
+from config import EMAIL, PASSWORD
+
+
+def get_client():
+  return openreview.Client(baseurl='https://api.openreview.net', username=EMAIL, password=PASSWORD)
+
+
+def papers2csv(final_papers, fpath):
+  all_papers = []
+  for venue, papers in final_papers.items():
+    venue_split = venue.split('/')
+    venue_name, venue_year, venue_type = venue_split[0], venue_split[1], venue_split[2]
+    for paper in papers:
+      paper['venue'] = venue_name
+      paper['year'] = venue_year
+      paper['type'] = venue_type
+      all_papers.append(paper)
+
+  field_names = list(all_papers[0].keys()) # choose one of the papers, get all the keys as they'll be same for rest of them
+
+  def write_csv():
+    with open(fpath, 'w') as fp:
+      writer = csv.DictWriter(fp, fieldnames=field_names)
+      writer.writeheader()
+      writer.writerows(all_papers)
+  write_csv()    
--- venue.py
+++ venue.py
@@ -1,1 +1,44 @@
-# all code related to venue like getting all the venues, out of which only required ones are kept
+def get_venues(client):
+  def filter_year(venue):
+    for year in years:
+      if year in venue:
+        return venue
+    return None
+  
+  venues = client.get_group(id='venues').members
+  venues = list(map(filter_year, venues))
+  venues = filter(lambda venue:venue is not None, venues)
+  reqd_venues = []
+
+  for venue in venues:
+    for conf in confs:
+      if conf.lower() in venue.lower():
+        reqd_venues.append(venue)
+        break
+
+  reqd_venues = map(filter_year, reqd_venues)
+  reqd_venues = list(filter(lambda venue:venue is not None, reqd_venues))
+
+  return reqd_venues
+
+
+def segregate_venues(venues, bins=['conference', 'workshop']):
+  # if a venue is not in any of the bins, then it is put into a misc bin
+  def get_bins_dict():
+    bins_dict = {bin:[] for bin in bins}
+    bins_dict['misc'] = []
+    return bins_dict
+  
+  bins_dict = get_bins_dict()
+  
+  for venue in venues:
+    binned = False
+    for bin in bins:
+      if bin.lower() in venue.lower():
+        bins_dict[bin].append(venue)
+        binned = True
+        break
+    if not binned:
+      bins_dict['misc'].append(venue)
+  
+  return bins_dict
