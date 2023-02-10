b19b6d23a46f1f417d4e8f6f1888585188cdd035
Pranav Sastry <pranava.sri@gmail.com>
2023-02-10 16:15:29 +0530

    added to csv as well... only integration testing needs to be done and also an example needs to be included

--- utils.py
+++ utils.py
@@ -5,22 +5,22 @@ def get_client():
   return openreview.Client(baseurl='https://api.openreview.net', username=EMAIL, password=PASSWORD)
 
 
-def papers2csv(final_papers, fpath):
+def papers_to_list(papers):
   all_papers = []
-  for venue, papers in final_papers.items():
-    venue_split = venue.split('/')
-    venue_name, venue_year, venue_type = venue_split[0], venue_split[1], venue_split[2]
-    for paper in papers:
-      paper['venue'] = venue_name
-      paper['year'] = venue_year
-      paper['type'] = venue_type
-      all_papers.append(paper)
+  for grouped_venues in papers:
+    for venue_papers in grouped_venues:
+      for paper in venue_papers:
+        all_papers.append(paper)
+  return all_papers
 
+
+def to_csv(papers, fpath):
+  all_papers = papers_to_list(papers)
   field_names = list(all_papers[0].keys()) # choose one of the papers, get all the keys as they'll be same for rest of them
-
+  
   def write_csv():
     with open(fpath, 'w') as fp:
       writer = csv.DictWriter(fp, fieldnames=field_names)
       writer.writeheader()
       writer.writerows(all_papers)
-  write_csv()    
+  write_csv()
