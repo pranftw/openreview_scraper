b8d859a94959e19b92ff278018a194b7a9c3728d
Pranav Sastry <pranava.sri@gmail.com>
2023-02-10 13:24:35 +0530

    filtering now returns matched keyword along with the filter typealso moved filter applying and satisfying filters to filter.py

--- filter.py
+++ filter.py
@@ -16,6 +16,30 @@ def check_keywords_with_text(keywords, text, threshold):
   return None, False
 
 
+  def apply_filters(papers, filters):
+    filtered_papers = {}
+    for group, grouped_venues in papers.items():
+      filtered_papers[group] = {}
+      for venue, venue_papers in grouped_venues:
+        filtered_papers[group][venue] = []
+        venue_filtered_papers = filtered_papers[group][venue]
+        for paper in venue_papers:
+          satisfying_keyword, satisfying_filter_type, satisfies = satisfies_any_filters(paper, filters)
+          # do something with all these satisfying keyword, satisfying filter, etc 
+          if satisfies:
+            venue_filtered_papers.append(paper)
+    return filtered_papers
+
+
+  def satisfies_any_filters(paper, filters):
+    for filter_, args, kwargs in filters:
+      matched_keyword, matched = filter_(paper, *args, **kwargs)
+      if matched:
+        filter_type = filter_.__name__
+        return matched_keyword, filter_type, True
+    return None, None, False
+
+
 def keywords_filter(paper, keywords, threshold=85):
   return check_keywords_with_keywords(keywords, paper.content['keywords'], threshold)
 
--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from utils import get_client
 from venue import get_venues, group_venues
 from paper import get_papers
+from filter import apply_filters
 
 
 class Scraper:
@@ -16,25 +17,7 @@ class Scraper:
   def execute(self):
     venues = get_venues(self.client, self.confs)
     papers = get_papers(self.client, group_venues(venues, self.groups), self.only_accepted)
-    filtered_papers = self.apply_filters(papers)
-  
-  def apply_filters(self, papers):
-    filtered_papers = {}
-    for group, grouped_venues in papers.items():
-      filtered_papers[group] = {}
-      for venue, venue_papers in grouped_venues:
-        filtered_papers[group][venue] = []
-        venue_filtered_papers = filtered_papers[group][venue]
-        for paper in venue_papers:
-          if self.satisfies_filters(paper):
-            venue_filtered_papers.append(paper)
-    return filtered_papers
-
-  def satisfies_filters(self, paper):
-    for filter_, args, kwargs in self.filters:
-      if not(filter_(paper, *args, **kwargs)):
-        return False
-    return True
+    filtered_papers = apply_filters(papers, self.filters)
 
   def add_filter(self, filter_, *args, **kwargs):
     self.filters.append((filter_, args, kwargs))
